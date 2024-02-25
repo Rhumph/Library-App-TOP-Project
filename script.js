76a8d5ed5f5983999688ea7fb16806c293277a0a
Night City <rileyjhumphreys@gmail.com>
2024-02-25 22:11:50 +1000

    started basic structure of project

---
+++ script.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+const newBook = document.getElementById("#book-name")
+submit-btn.addEventListener()
+
+function Book(author, title, genre, readStatus) {
+    this.author = author;
+    this.title = title;
+    this.genre = genre;
+    this.readStatus = readStatus;
+}
+
+
+
+function addBookToLibrary() {
+  const newBook = new Book.name;
+  myLibrary.push(newBook);
+}
+
+console.log(myLibrary)
