9b732cef2b02d7d93ceff60a6c9db60a1f392292
Night City <rileyjhumphreys@gmail.com>
2024-03-06 22:33:44 +1000

    Had it working with arrays, replaced with objects, now need to debug

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
+// const myLibrary = [
+//     ["Chaos", "Tom O'Neill", "Investigative Journalism", "528", "Have Read"],
+//     ["Breath", "James Nestor", "Health/Investigative", "304", "Have Read"],
+//     ["The Gulag archipelago", "Aleksandr Solzhenitsyn", "History/Politics", "1948", "Currently Reading"],
+//     ["1984", "George Orwell", "Dystopian Fiction", "328", "Have Read"]
+// ];
+
 const myLibrary = [
-    ["Chaos", "Tom O'Neill", "Investigative Journalism", "528", "Have Read"],
-    ["Breath", "James Nestor", "Health/Investigative", "304", "Have Read"],
-    ["The Gulag archipelago", "Aleksandr Solzhenitsyn", "History/Politics", "1948", "Currently Reading"],
-    ["1984", "George Orwell", "Dystopian Fiction", "328", "Have Read"]
+    {
+        title: "Chaos",
+        author: "Tom O'Neill",
+        genre: "Investigative Journalism",
+        length: "528",
+        readStatus: "Have Read"
+    },
+    {
+        title: "Breath",
+        author: "James Nestor",
+        genre: "Health/Investigative",
+        length: "304",
+        readStatus: "Have Read"
+    },
+    {
+        title: "The Gulag archipelago",
+        author: "Aleksandr Solzhenitsyn",
+        genre: "History/Politics",
+        length: "1948",
+        readStatus: "Currently Reading"
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        genre: "Dystopian Fiction",
+        length: "328",
+        readStatus: "Have Read"
+    }
 ];
+
 
 // bif elements
 const newBookTitle = document.getElementById("bookTitle");
@@ -19,47 +51,48 @@ function book(bookTitle, bookAuthor, bookGenre, bookLength, bookReadStatus) {
     this.bookTitle = bookTitle;
     this.bookAuthor = bookAuthor;
     this.bookGenre = bookGenre;
-    this.bookLength = [bookLength + " Pages"];
+    this.bookLength = bookLength + " Pages";
     this.bookReadStatus = bookReadStatus;
 
 }
 
+book.prototype.bookInfoArray = function () {
+    return [this.bookTitle, this.bookAuthor, this.bookGenre, this.bookLength, this.bookReadStatus];
+}
 
 // Function that builds a card for each book
-function createBookCards (book){ 
+function createBookCards(title, author, genre, length, readstatus) {
     const bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
 
     const bTitle = document.createElement("h3");
     bTitle.classList.add("BCtitle");
-    bTitle.textContent = [book[0]];
+    bTitle.textContent = title;
 
     const bAuthor = document.createElement("p");
     bAuthor.classList.add("BCauthor");
-    bAuthor.textContent = [book[1]];
+    bAuthor.textContent = book.bookAuthor;
 
     const bGenre = document.createElement("p");
     bGenre.classList.add("BCgenre");
-    bGenre.textContent = [book[2]];
+    bGenre.textContent = book.bookGenre;
 
     const bLength = document.createElement("p");
     bLength.classList.add("BCLength");
-    bLength.textContent = [book[3]]
+    bLength.textContent = book.bookLength;
 
     const bReadStatus = document.createElement("p");
     bReadStatus.classList.add("BCreadstatus");
-    bReadStatus.textContent = [book[4]]
+    bReadStatus.textContent = book.bookReadStatus;
 
     bookCard.appendChild(bTitle);
     bookCard.appendChild(bAuthor);
     bookCard.appendChild(bGenre);
     bookCard.appendChild(bLength);
     bookCard.appendChild(bReadStatus);
-    
+
     const bookCards = document.getElementById("main-content");
     bookCards.appendChild(bookCard);
-
-    
 
 }
 
@@ -67,22 +100,19 @@ function createBookCards (book){
 
 function addBookToLibrary() {
 
-    const bookName = newBookTitle.textContent;
+    const bookName = newBookTitle.value;
     const bookAuthor = newBookAuthor.value;
     const bookGenre = newBookGenre.value;
     const bookLength = newBookLength.value;
     const bookReadStatus = newBookReadStatus.value;
 
 
-
+    // [bookName, bookAuthor, bookGenre, bookLength, bookReadStatus];
     const bookToAdd = new book(bookName, bookAuthor, bookGenre, bookLength, bookReadStatus);
     myLibrary.push(bookToAdd);
-    createBookCards(bookToAdd);
-
-    // myLibrary.forEach(book => {
-    //     console.log(book.bookTitle);
-    // });
+    bookToAdd.bookInfoArray()
+    createBookCards(myLibrary[myLibrary.length - 1]);
 
 }
 
-submitBtn.addEventListener("click", addBookToLibrary);
+submitBtn.addEventListener("click", addBookToLibrary, console.log(myLibrary));
